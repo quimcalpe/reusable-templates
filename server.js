14e6c35de470c7ff9353911e5e012e5bd7a68192
Quim Calpe <quim@kalpe.com>
2013-11-15 13:54:35 +0100

    Modularize tpl-helpers

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-var handlebars = require("handlebars");
-var fs = require('fs');
 var express = require("express");
-var Q = require('q'); // promises
-
-var tpl_cache = {};
-var tpl_partials_cache = {};
+var TPL = require("./tpl-helpers");
 
 var app = express();
 app.use(express.bodyParser());
@@ -14,15 +9,15 @@ app.get('/:section?', function (req, res) {
     req.params.section = "home";
   }
   if ( req.headers['x-ajax'] ) {
-    load_template(req.params.section).then(function (tpl) {
+    TPL.load(req.params.section).then(function (tpl) {
       res.send(tpl());
     }, function (error) {
       console.error(error);
       res.send(404, '<h1>404 not found</h1>');
     });
   } else {
-    load_template("index").then(function (tpl) {
-      return register_partial("content", req.params.section, tpl);
+    TPL.register_partial("content", req.params.section).then(function (){
+      return TPL.load("index");
     }).then(function (tpl) {
       res.send(tpl());
     }, function (error) {
@@ -38,37 +33,3 @@ app.use(function (err, req, res, next) {
 });
 app.listen(3000);
 console.log('Listening on port 3000');
-
-function load_template(template) {
-  var deferred = Q.defer();
-  if ( tpl_cache[template] ) {
-      deferred.resolve(tpl_cache[template]);
-  } else {
-    fs.readFile('public/tpl/'+template+'.tpl', 'utf8', function (err, data) {
-      if ( err ) {
-        deferred.reject(err);
-      } else {
-        tpl = handlebars.compile(data);
-        tpl_cache[template] = tpl;
-        deferred.resolve(tpl);        
-      }
-    });
-  }
-  return deferred.promise;
-}
-
-function register_partial(partial, template, pare) {
-  var deferred = Q.defer();
-  if ( tpl_partials_cache[partial] === template ) {
-      deferred.resolve(pare);
-  } else {
-    load_template(template).then(function (tpl) {
-      handlebars.registerPartial(partial, tpl);
-      tpl_partials_cache[partial] = template;
-      deferred.resolve(pare);
-    }, function (error) {
-      deferred.reject(error);
-    });
-  }
-  return deferred.promise;
-}
---
+++ tpl-helpers.js
@@ -0,0 +1,45 @@
+var handlebars = require("handlebars");
+var fs = require('fs');
+var Q = require('q'); // promises
+
+var tpl_cache = {};
+var tpl_partials_cache = {};
+
+function load(template, no_cache) {
+  var deferred = Q.defer();
+  if ( no_cache !== true && tpl_cache[template] ) {
+      deferred.resolve(tpl_cache[template]);
+  } else {
+    fs.readFile('public/tpl/'+template+'.tpl', 'utf8', function (err, data) {
+      if ( err ) {
+        deferred.reject(err);
+      } else {
+        tpl = handlebars.compile(data);
+        tpl_cache[template] = tpl;
+        deferred.resolve(tpl);        
+      }
+    });
+  }
+  return deferred.promise;
+}
+
+function register_partial(partial, template, no_cache) {
+  var deferred = Q.defer();
+  if ( no_cache !== true && tpl_partials_cache[partial] === template ) {
+      deferred.resolve(true);
+  } else {
+    load(template).then(function (tpl) {
+      handlebars.registerPartial(partial, tpl);
+      tpl_partials_cache[partial] = template;
+      deferred.resolve(true);
+    }, function (error) {
+      deferred.reject(error);
+    });
+  }
+  return deferred.promise;
+}
+
+module.exports = {
+	load : load,
+	register_partial : register_partial
+};
