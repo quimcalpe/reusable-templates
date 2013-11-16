8dad01906859a548c0909e8bfd460b459fe33f24
Quim Calpe <quim@kalpe.com>
2013-11-16 01:03:47 +0100

    Fix t.equal order

--- test/throttling.js
+++ test/throttling.js
@@ -2,16 +2,17 @@ var test = require('tap').test;
 var throttle = require('../throttle');
 
 test('change to server on 10th request', function (t) {
-  req = {};
-  throttle(req, {}, function(){}); //0
+  var req = {};
+  var next = function(){};
+  throttle(req, {}, next); //0
   t.equal(req.render_mode, "browser");
   for (var i=1; i<=10; i++) {
-    throttle(req, {}, function(){}); //1->10
+    throttle(req, {}, next); //1->10
   }
   t.equal(req.render_mode, "browser");
-  throttle(req, {}, function(){}); //11
+  throttle(req, {}, next); //11
   t.equal(req.render_mode, "server");
-  throttle(req, {}, function(){}); //12
+  throttle(req, {}, next); //12
   t.equal(req.render_mode, "server");
   t.end();
 });
