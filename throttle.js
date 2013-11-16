fad5a1482b9634439055825911e00e5d9f5111db
Quim Calpe <quim@kalpe.com>
2013-11-16 01:06:33 +0100

    Add console log when throttling

--- throttle.js
+++ throttle.js
@@ -5,9 +5,10 @@ var count = 0;
 module.exports = function (req, res, next) {
   // simulate server load, on 10th request, change to server rendering
   if ( count > 10 ) {
+    console.log("Hight server load! changing render mode to 'server'");
     req.render_mode = "server";
   } else {
-    req.render_mode = "browser";    
+    req.render_mode = "browser";
   }
   count++;
   next();
