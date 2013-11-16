1861c293d2e601ea40800977aa56ba8d769e0d89
Quim Calpe <quim@kalpe.com>
2013-11-16 01:08:18 +0100

    Remove console log when throttling

--- throttle.js
+++ throttle.js
@@ -5,7 +5,6 @@ var count = 0;
 module.exports = function (req, res, next) {
   // simulate server load, on 10th request, change to server rendering
   if ( count > 10 ) {
-    console.log("Hight server load! changing render mode to 'server'");
     req.render_mode = "server";
   } else {
     req.render_mode = "browser";
